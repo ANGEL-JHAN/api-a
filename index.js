437b45a61be6cfcfcf91dbe85aaed59c52ab49f7
ANGEL-JHAN <62525883angel@gmail.com>
2026-07-07 18:22:52 -0500

    Actualizar index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import { createClient } from "@supabase/supabase-js";
 dotenv.config();
 
 const app = express();
+
 app.use(express.json());
 
 const supabase = createClient(
@@ -12,6 +13,8 @@ const supabase = createClient(
   process.env.SUPABASE_KEY
 );
 
+
+// Estado de la API
 app.get("/", (req, res) => {
   res.json({
     online: true,
@@ -19,9 +22,12 @@ app.get("/", (req, res) => {
   });
 });
 
+
+// Ver datos de prueba
 app.get("/data", async (req, res) => {
+
   const { data, error } = await supabase
-    .from("datos")
+    .from("licenses")
     .select("*");
 
   if (error) {
@@ -33,8 +39,106 @@ app.get("/data", async (req, res) => {
   res.json(data);
 });
 
+
+// Verificar licencia
+app.post("/verify-license", async (req, res) => {
+
+  try {
+
+    const { key, device_id } = req.body;
+
+
+    if (!key) {
+      return res.json({
+        valid: false,
+        msg: "Falta la key"
+      });
+    }
+
+
+    const { data, error } = await supabase
+      .from("licenses")
+      .select("*")
+      .eq("license_key", key)
+      .single();
+
+
+    if (error || !data) {
+      return res.json({
+        valid: false,
+        msg: "Key no existe"
+      });
+    }
+
+
+    if (!data.active) {
+      return res.json({
+        valid: false,
+        msg: "Key bloqueada"
+      });
+    }
+
+
+    if (data.expires_at) {
+
+      const fecha = new Date(data.expires_at);
+      const ahora = new Date();
+
+      if (fecha < ahora) {
+        return res.json({
+          valid: false,
+          expired: true,
+          msg: "Key expirada"
+        });
+      }
+
+    }
+
+
+    // Guardar dispositivo la primera vez
+    if (!data.device_id) {
+
+      await supabase
+        .from("licenses")
+        .update({
+          device_id: device_id
+        })
+        .eq("id", data.id);
+
+    }
+
+
+    else if (data.device_id !== device_id) {
+
+      return res.json({
+        valid: false,
+        msg: "Key usada en otro dispositivo"
+      });
+
+    }
+
+
+    return res.json({
+      valid: true,
+      msg: "Key válida"
+    });
+
+
+  } catch (err) {
+
+    res.status(500).json({
+      error: err.message
+    });
+
+  }
+
+});
+
+
+
 const PORT = process.env.PORT || 3000;
+
 
 app.listen(PORT, () => {
   console.log("Servidor iniciado en puerto " + PORT);
-}); 
+});
